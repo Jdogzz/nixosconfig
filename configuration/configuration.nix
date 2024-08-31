d6639ae4bc3d276cd0384b94c6e99c82ad3fa975
flakeuser <jerzor@pacbell.net>
2024-08-31 14:50:46 -0700

    Add line to remove pulseaudio.

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -71,6 +71,8 @@
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
+    hardware.pulseaudio.enable = false;
+
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
