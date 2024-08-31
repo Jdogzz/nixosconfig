6228029183ab95b9edc87db4357a17f9a7be4240
flakeuser <jerzor@pacbell.net>
2024-08-31 14:38:13 -0700

    Add another line for kde from wiki.

--- configuration/configuration-kde.nix
+++ configuration/configuration-kde.nix
@@ -7,6 +7,7 @@
 
 {
   programs.partition-manager.enable = true;
+  services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
