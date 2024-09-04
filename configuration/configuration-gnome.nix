f6d055dc7ce8a155e2a008bc9495a78bfc383b58
flakeuser <jerzor@pacbell.net>
2024-09-04 08:12:08 -0700

    Switch temporarily to lightdm.

--- configuration/configuration-gnome.nix
+++ configuration/configuration-gnome.nix
@@ -7,7 +7,11 @@
 
 {
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.displayManager.lightdm = {
+    enable = true;
+    greeters.gtk.enable = true;
+  };
   services.xserver.desktopManager.gnome.enable = true;
 }
