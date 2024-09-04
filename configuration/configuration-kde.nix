f36b512ff02c013dabf5da4dfe81262e89587e89
flakeuser <jerzor@pacbell.net>
2024-09-04 08:21:31 -0700

    Switch KDE config to lightdm.

--- configuration/configuration-kde.nix
+++ configuration/configuration-kde.nix
@@ -8,7 +8,11 @@
 {
   programs.partition-manager.enable = true;
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.lightdm = {
+    enable = true;
+    greeters.gtk.enable = true;
+  };
   services.desktopManager.plasma6.enable = true;
 }
