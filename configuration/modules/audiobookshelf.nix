977a1b6ff606a65c7fb34ea8ec0ac15400b51330
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-03-04 13:45:34 -0800

    Write audiobookshelf service manually.

--- configuration/modules/audiobookshelf.nix
+++ configuration/modules/audiobookshelf.nix
@@ -9,11 +9,32 @@
   environment.systemPackages = [
     pkgs.audiobookshelf
   ];
-  services.audiobookshelf = {
-    enable = true;
-    user = "flakeuser";
-    openFirewall = true;
-    host = "0.0.0.0";
-    dataDir = "/home/flakeuser/servicesync/audiobookshelf";
+
+  systemd.services.audiobookshelf = {
+    description = "Audiobookshelf is a self-hosted audiobook and podcast server";
+
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "flakeuser";
+      Group = "users";
+      StateDirectory = "audiobookshelf";
+      WorkingDirectory = "/home/flakeuser/servicesync/audiobookshelf";
+      ExecStart = "${pkgs.audiobookshelf}/bin/audiobookshelf --host 0.0.0.0 --port 8000";
+      Restart = "on-failure";
+    };
   };
+
+  networking.firewall.allowedTCPPorts = [ 8000 ];
+
+  #2025-03-04 The nixos provided service requires running in /var/lib so I'm extracting the basics to run it myself.
+  # services.audiobookshelf = {
+  #   enable = true;
+  #   user = "flakeuser";
+  #   openFirewall = true;
+  #   host = "0.0.0.0";
+  #   dataDir = "/home/flakeuser/servicesync/audiobookshelf";
+  # };
 }
