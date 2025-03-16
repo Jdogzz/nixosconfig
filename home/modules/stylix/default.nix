a54c8e9273c7bb175779cea00a0db16533353496
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-03-15 20:43:00 -0700

    Generate colorscheme from wallpaper.

--- home/modules/stylix/default.nix
+++ home/modules/stylix/default.nix
@@ -8,9 +8,10 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
-    image = ../../wallpaper.jpg;
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
+    image = ./wallpaper.jpg;
     opacity.terminal = 0.8;
+    polarity = "dark";
     targets = {
       hyprlock = {
         useWallpaper = true;
