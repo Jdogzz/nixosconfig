07831952d12c059cce2301a0811834f71b323cfb
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-03-31 07:25:37 -0700

    Switch back to previous key setup.

--- modules/openssh.nix
+++ modules/openssh.nix
@@ -8,7 +8,7 @@
 {
   users.users.flakeuser = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEpS99mQv9a9BQE59CGlKE0xcZnIp6qalqTXVSvV34fN flakeuser"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEpS99mQv9a9BQE59CGlKE0xcZnIp6qalqTXVSvV34fN"
     ];
   };
 
