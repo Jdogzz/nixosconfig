7681cfe0bb97e2bc0c244a5cd40bc4164373063b
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-03-31 20:38:13 -0700

    Change default monospace font.

--- home/modules/stylix/default.nix
+++ home/modules/stylix/default.nix
@@ -9,6 +9,7 @@
   stylix = {
     enable = true;
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
+    fonts.monospace.package = pkgs.nerd-fonts.jetbrains-mono;
     image = ./wallpaper.jpg;
     opacity = {
       applications = 0.8;
