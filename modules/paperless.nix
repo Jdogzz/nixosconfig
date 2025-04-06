e76ad25f143ea985635c08f74164f35579f58117
Sandro <sandro.jaeckel@gmail.com>
2025-04-07 00:23:09 +0200

    Drop noop ocr flags
    optimize 1 is the default and without pdfa there is no image compression https://github.com/ocrmypdf/OCRmyPDF/blob/8b1443c4826e8ebe4556c8b0d1cdfc31bc83c3c8/src/ocrmypdf/builtin_plugins/ghostscript.py#L87-L88

--- modules/paperless.nix
+++ modules/paperless.nix
@@ -16,10 +16,6 @@
     settings = {
       PAPERLESS_OCR_OUTPUT_TYPE = "pdf"; # 2025-02-28 Adding this in to preserve hyperlinks in PDFs generated
       PAPERLESS_OCR_LANGUAGE = "eng";
-      PAPERLESS_OCR_USER_ARGS = {
-        optimize = 1;
-        pdfa_image_compression = "lossless";
-      };
       PAPERLESS_TIKA_ENABLED = true;
       PAPERLESS_GOTENBERG_ENABLED = true;
     };
