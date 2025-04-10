c424821c1a3ea80a63f1c20b2f6e870499e3a9ee
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-04-10 07:21:38 -0700

    Change wallpaper.

--- home/modules/stylix/default.nix
+++ home/modules/stylix/default.nix
@@ -13,7 +13,7 @@
       package = pkgs.nerd-fonts.jetbrains-mono;
       name = "JetBrainsMono Nerd Font";
     };
-    image = ./wallpaper.jpg;
+    image = ./wallpaper.png;
     opacity = {
       applications = 0.8;
       desktop = 0.8;
