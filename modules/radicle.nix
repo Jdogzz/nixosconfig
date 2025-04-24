29aed35710ed0de59a65b16844c51dd86dc151a9
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-04-24 08:00:28 -0700

    Set key paths.

--- modules/radicle.nix
+++ modules/radicle.nix
@@ -9,5 +9,7 @@
   services.radicle = {
     enable = true;
     node.openFirewall = true;
+    privateKeyFile = "/home/flakeuser/.radicle/keys/radicle";
+    publicKey = "/home/flakeuser/.radicle/keys/radicle.pub";
   };
 }
