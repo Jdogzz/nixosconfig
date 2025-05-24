bba961593dfe967f04d62d773169304f8d093564
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-05-24 07:16:37 -0700

    Add ffmpegthumbnailer.

--- home/modules/emacs/modules/emacs/dired/default.nix
+++ home/modules/emacs/modules/emacs/dired/default.nix
@@ -7,6 +7,7 @@
 
 {
   home.packages = with pkgs; [
+    ffmpegthumbnailer # Needed for dirvish image thumbnail display
     poppler-utils # Needed for dirvish pdf display and pdf-tools
     vips # Needed for dirvish image display
   ];
